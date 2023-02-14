c099b2f3d83065ca452f9b55a5ea40253b4df93d
Vinicius Soares <viniciussoaresmbr@gmail.com>
2023-02-14 00:18:23 -0300

    Adicionando username

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { UserContext } from '../../contexts/User';
 import DropDownMenu from '../DropDownMenu';
 import { Styled } from './styles';
 
 const ProfileCard = () => {
+  const { username, getUserById } = useContext(UserContext);
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
   const [showMenu, setShowMenu] = useState(false);
   return (
     <React.Fragment>
       <Styled.ProfileCard onClick={() => setShowMenu(!showMenu)}>
         <Styled.UserProfile />
-        <Styled.Username>Vinicius Soares</Styled.Username>
+        <Styled.Username>{username}</Styled.Username>
       </Styled.ProfileCard>
       <DropDownMenu showMenu={showMenu} />
     </React.Fragment>
