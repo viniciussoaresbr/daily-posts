0675177f86ea92cb326f0bdf0753c1bddc9b89a4
Vinicius Soares <viniciussoaresmbr@gmail.com>
2026-03-24 14:02:21 -0300

    fix(api): bind auth and user controller handlers

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,19 @@
-import { NextFunction, Request, Response } from 'express';
-import { IAuthController, IAuthService } from '../interfaces';
+import { NextFunction, Request, Response } from "express";
+import { IAuthController, IAuthService } from "../interfaces";
 
 class AuthController implements IAuthController {
-  constructor(private authService: IAuthService) {}
+  constructor(private authService: IAuthService) {
+    this.auth = this.auth.bind(this);
+  }
 
-  auth = async (req: Request, res: Response, next: NextFunction) => {
+  async auth(req: Request, res: Response, next: NextFunction) {
     try {
       const data = await this.authService.auth(req.body);
       res.status(200).send(data);
     } catch (error) {
       next(error);
     }
-  };
+  }
 }
 
 export default AuthController;
--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,19 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
-import { IUserController, IUserService } from '../interfaces';
+import { NextFunction, Request, Response } from "express";
+import { IUserController, IUserService } from "../interfaces";
 
 class UserController implements IUserController {
-  constructor(private userService: IUserService) {}
+  constructor(private userService: IUserService) {
+    this.save = this.save.bind(this);
+    this.findUserById = this.findUserById.bind(this);
+  }
 
-  save = async (req: Request, res: Response, next: NextFunction) => {
+  async save(req: Request, res: Response, next: NextFunction) {
     try {
       await this.userService.save(req.body);
-      res.status(201).send({ message: 'Usuário criado com sucesso' });
+      res.status(201).send({ message: "Usuário criado com sucesso" });
     } catch (error) {
       next(error);
     }
-  };
+  }
 
-  findUserById = async (req: Request, res: Response, next: NextFunction) => {
+  async findUserById(req: Request, res: Response, next: NextFunction) {
     try {
       const data = await this.userService.findUserById(
         parseInt(req.params.userId),
@@ -22,7 +25,7 @@ class UserController implements IUserController {
     } catch (error) {
       next(error);
     }
-  };
+  }
 }
 
 export default UserController;
