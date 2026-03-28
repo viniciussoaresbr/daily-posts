1463d9f34a1674bd6c559bd4fc86a2af6a9cdd90
Vinicius Soares <viniciussoaresmbr@gmail.com>
2026-03-28 20:29:55 -0300

    feat(frontend): add message-circle icon

--- frontend/src/app/shared/components/icon/icon.component.ts
+++ frontend/src/app/shared/components/icon/icon.component.ts
@@ -24,6 +24,11 @@ import { Component, Input } from '@angular/core';
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
       </svg>
 
+      <!-- message-circle -->
+      <svg *ngSwitchCase="'message-circle'" [class]="class" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+      </svg>
+
       <!-- empty-posts -->
       <svg *ngSwitchCase="'empty-posts'" [class]="class" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10l4 4v10a2 2 0 01-2 2z" />
